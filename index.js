142e22e86401728f234afb9e789288d464a70e9f
caitlin <caitlin.wink@gmail.com>
2021-03-27 18:45:24 -0700

    commit tree c062817ef25ed4baca980ff2b077770a2f2aa43e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function superbowlWin(finder){
+    for (const record of finder)
+        if(record.result === "W"){
+    return(record.year)
+    }
+}
+console.log(record.find(superbowlWin))
 
+// function blueFilter (collection) {
+//     for (const user of collection) {
+//       if (user.favoriteColor === 'Blue') {
+//         console.log(user.firstName);
+//       }
+//     }
+//   }
